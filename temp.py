14e9a26449afa720b8e466fdca73bcb2965145ed
RabbleRabble <mjfisher31@yahoo.com>
2016-01-13 00:30:31 +0000

    first commit

--- temp.py
+++ temp.py
@@ -8,11 +8,11 @@ sense.set_rotation(180)
 sense.clear()
 
 
-def fix_temp(temp, cpu, r, g, b):
+def fix_temp(temp, cpu, r, g, b, rb, gb, bb):
     temp = ((temp - ((cpu - temp)/2))-6)
     temp = (temp * 1.8) + 32
     temp = round(temp)
-    sense.show_message("T:" + str(temp), text_colour=[r, g, b])
+    sense.show_message("T:" + str(temp), text_colour=[r, g, b], back_colour=[rb, gb, bb])
 
 
 while True:
@@ -30,11 +30,26 @@ while True:
     g = random.randint(0, 255)
     b = random.randint(0, 255)
 
+    rb = random.randint(0, 255)
+    gb = random.randint(0, 255)
+    bb = random.randint(0, 255)
+
+    while (r==rb and g==gb and b==bb) or (r==0 and g==0 and b==0):
+        if r==rb and g==gb and b==bb:
+            rg = random.randint(0, 255)
+            gb = random.randint(0, 255)
+            bb = random.randint(0, 255)
+        elif r==0 and g==0 and b==0:
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            
+        
     hum = sense.get_humidity()
     hum = round(hum)
 
-    fix_temp(temp, cputemp, r, g, b)
+    fix_temp(temp, cputemp, r, g, b, rb, gb, bb)
 
-    sense.show_message("H:" + str(hum), text_colour=[r, g, b])
+    sense.show_message("H:" + str(hum), text_colour=[r, g, b], back_colour=[rb, gb, bb])
 
     time.sleep(4)
